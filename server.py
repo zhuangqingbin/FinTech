b7a866490288c953ebb8465cfbb8ae4f2e42dbb9
MacJimmy <360650538@qq.com>
2020-05-10 20:51:35 +0800

    fd

--- server.py
+++ server.py
@@ -6,15 +6,16 @@
 
 import smtplib
 from email.mime.text import MIMEText
-def sendEmail(content):
-	msg=MIMEText(content,'plain','utf-8')
-	msg['From'] = '发送'
-	msg['To'] = '接受'
-	msg['Subject'] = '题目'
 
-	server=smtplib.SMTP('smtp.qq.com',25)
-	server.set_debuglevel(1)
-	server.login("360650538@qq.com",'velkooyixtgwbgbb')
-	server.sendmail('360650538@qq.com',['360650538@qq.com'],msg.as_string())
-	server.quit()
-sendEmail('Hello')
+msg=MIMEText('Hello','plain','utf-8')
+msg['From'] = '发送'
+msg['To'] = '接受'
+msg['Subject'] = '题目'
+
+server=smtplib.SMTP_SSL('smtp.qq.com'', 465)
+# server=smtplib.SMTP('smtp.qq.com',25)
+server.set_debuglevel(1)
+server.login("360650538@qq.com",'velkooyixtgwbgbb')
+server.sendmail('360650538@qq.com',['360650538@qq.com'],msg.as_string())
+server.quit()
+
